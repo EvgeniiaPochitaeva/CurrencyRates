94e0d05b8edbd86c9a7512876788c9ee48bcb3b8
Anton Vasylevskyi <vasilevskiy@gmail.com>
2023-10-23 22:27:29 +0300

    Update dependencies to the correct values

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,13 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
 
     // https://mvnrepository.com/artifact/org.telegram/telegrambots
-    implementation("org.telegram:telegrambots:6.3.0")
+    implementation("org.telegram:telegrambots:6.8.0")
+
+    implementation("org.slf4j:slf4j-api:1.7.32")
+    implementation("ch.qos.logback:logback-classic:1.2.9")
+    implementation("org.quartz-scheduler:quartz:2.3.2")
+    implementation("javax.json:javax.json-api:1.1.4")
+    implementation("org.glassfish:javax.json:1.1.4")
 
     // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind
     implementation("com.fasterxml.jackson.core:jackson-databind:2.15.3")
